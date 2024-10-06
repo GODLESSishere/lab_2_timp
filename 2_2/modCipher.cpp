ac9f26764632435ef68d96eb037cbf1d011b8d88
GODLESSishere <gimmethelootanonimous@gmail.com>
2024-10-06 14:06:55 +0300

    Update modCipher.cpp

--- 2_2/modCipher.cpp
+++ 2_2/modCipher.cpp
@@ -1,29 +1,34 @@
 #include "modCipher.h"
+#include <iostream>
+#include <algorithm> 
 
 modCipher::modCipher(int skey, string text) {
+    if (text.empty())
+        throw cipher_error("Отсутствует открытый текст!");
     key = getValidKey(skey, text);
 }
 
 string modCipher :: encryption(string &text) {
     string t = getValidOpenText(text);
-    int k = 0;
     int simvoli = t.size();
-    int stroki = ((simvoli) / key);
+    int stroki = (simvoli + key - 1) / key;
     char** tabl = new char* [stroki];
     for (int i = 0; i < stroki; i++)
         tabl[i] = new char [key];
     for (int i = 0; i < stroki; i++)
         for (int j = 0; j < key; j++) {
-            if(k < simvoli) {
-                tabl[i][j] = t[k];
-                k++;
+            if(i * key + j < simvoli) {
+                tabl[i][j] = t[i * key + j];
+            } else {
+                tabl[i][j] = ' ';
             }
         }
-    k = 0;
-    for (int j = key - 1; j >= 0 ; j--)
+    t.clear();
+    for (int j = 0; j < key; j++)
         for (int i = 0; i < stroki; i++) {
-            t[k] = tabl[i][j];
-            k++;
+            if(tabl[i][j] != ' ') { 
+                t += tabl[i][j];
+            }
         }
     for (int i = 0; i < stroki; i++)
         delete[] tabl[i];
@@ -32,31 +37,38 @@ string modCipher :: encryption(string &text) {
 }
 
 inline string modCipher::getValidCipherText(string &s, string &open_text) {
+    s.erase(std::remove(s.begin(), s.end(), ' '), s.end());
     int r1 = s.size();
     int r2 = open_text.size();
-    if (r1 != r2)
-        throw cipher_error (string ("Неправельный зашифрованный текст: ") + s);
+    if (r1 != r2) {
+        std::cerr << "Предупреждение: Длина зашифрованного текста не соответствует длине открытого текста." << std::endl;
+        return s; 
+    }
     return s;
 }
 
 string modCipher :: transcript(string &text, string &open_text) {
     string t = getValidCipherText(text, open_text);
-    int k = 0;
+    t.erase(std::remove(t.begin(), t.end(), ' '), t.end());
     int simvoli = t.size();
-    int stroki = ((simvoli) / key);
+    int stroki = (simvoli + key - 1) / key;
     char** tabl = new char* [stroki];
     for (int i = 0; i < stroki; i++)
         tabl[i] = new char [key];
-    for (int j = key - 1; j >= 0 ; j--)
+    for (int j = 0; j < key; j++)
         for (int i = 0; i < stroki; i++) {
-            tabl[i][j] = t[k];
-            k++;
+            if(j * stroki + i < simvoli) {
+                tabl[i][j] = t[j * stroki + i];
+            } else {
+                tabl[i][j] = ' ';
+            }
         }
-    k = 0;
+    t.clear();
     for (int i = 0; i < stroki; i++)
         for (int j = 0; j < key; j++) {
-            t[k] = tabl[i][j];
-            k++;
+            if(tabl[i][j] != ' ') { 
+                t += tabl[i][j];
+            }
         }
     for (int i = 0; i < stroki; i++)
         delete[] tabl[i];
@@ -65,22 +77,22 @@ string modCipher :: transcript(string &text, string &open_text) {
 }
 
 inline string modCipher::getValidOpenText(string &s) {
+    if (s.empty())
+        throw cipher_error("Отсутствует открытый текст!");
     int razmer = s.size();
     for(int i = 0; i < razmer; i++)
         if (s[i] == ' ') {
             s.erase(i, 1);
             i--;
         }
-    if (s.empty())
-        throw cipher_error("Отсутствует открытый текст!");
     for(int i = 0; i < razmer; i++)
-        if(s[i] < 'A' || s[i] > 'z')
+        if(s[i] < 'A' ||  s[i] > 'z')
             throw cipher_error("В тексте встречены некорректные символы!");
     return s;
 }
 
 inline int modCipher::getValidKey(int key, const string &Text) {
     if(key < 2 || key > static_cast<int>(Text.size()))
-        throw cipher_error("Ключ некоректного размера");
+        throw cipher_error("Ключ некорректного размера");
     return key;
 }
